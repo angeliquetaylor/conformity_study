ce850ca19a2b7cff1a08ba0dd5e75d6ea7370f0c
angeliquetaylor <amt062@eng.ucsd.edu>
2017-05-10 10:24:26 -0700

    first commit

---
+++ devel/share/skeleton_tracker/cmake/skeleton_tracker-msg-paths.cmake
@@ -0,0 +1,4 @@
+# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
+
+set(skeleton_tracker_MSG_INCLUDE_DIRS "/home/angelique/ros_catkin_ws/src/skeleton_tracker/msg")
+set(skeleton_tracker_MSG_DEPENDENCIES geometry_msgs)
